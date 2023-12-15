170a0c69710f673d9790f206b55d98478089faa8
Dev <devextraemail@gmail.com>
2023-12-14 19:12:47 -0500

    mainpage fixed on home components

---
+++ client/daia_client/src/containers/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../containers/styles/MainPage.css";
+
+function MainPage() {
+    return (
+      <div className="main-page">
+        <h1>DAIA Dashboard</h1>
+      </div>
+    );
+}
+
+export default MainPage;
