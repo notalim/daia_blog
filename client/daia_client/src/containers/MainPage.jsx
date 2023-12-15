cb3811c5bdec63fa674b7897ebcc82dbb9ccc9c0
Dev <devextraemail@gmail.com>
2023-12-14 19:17:41 -0500

    content on main page

--- client/daia_client/src/containers/MainPage.jsx
+++ client/daia_client/src/containers/MainPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../containers/styles/MainPage.css";
+import EmergencyContactGrid from "../components/EmergencyContactsGrid";
 
 function MainPage() {
-    return (
-      <div className="main-page">
-        <h1>DAIA Dashboard</h1>
+  return (
+    <div className="main-page bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">DAIA Dashboard</h1>
+      <div className="main-page-content">
+        <div className="main-page-content-right">
+          <EmergencyContactGrid />
+        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default MainPage;
